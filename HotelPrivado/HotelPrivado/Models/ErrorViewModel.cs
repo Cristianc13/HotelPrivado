e919057ec46f26a0a71da63b528aa3f5df5fe572
Cristian Canales <55901424+Cristianc13@users.noreply.github.com>
2019-12-09 17:01:06 -0600

    Creacion del proyecto

---
+++ HotelPrivado/HotelPrivado/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotelPrivado.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
