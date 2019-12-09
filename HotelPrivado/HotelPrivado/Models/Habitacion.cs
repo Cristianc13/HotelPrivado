4e89c783ea624814cdc8898a7aa7aa103a0e8920
Alberto Jose Blandon Moreno <ialbertbla98@gmail.com>
2019-12-09 17:48:27 -0600

    modelos de Habitacion y tipo de habitacion

---
+++ HotelPrivado/HotelPrivado/Models/Habitacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelPrivado.Models
+{
+    public class Habitacion
+    {
+        [Key]
+        public int idHabitacion { get; set; }
+        public int NumeroCamas { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public string Observacion { get; set; }
+
+        [ForeignKey("Tipo")]
+        public int idTipo { get; set; }
+        [ForeignKey("idTipo")]
+        public virtual Tipo Tipo { get; set; }
+    }
+}
