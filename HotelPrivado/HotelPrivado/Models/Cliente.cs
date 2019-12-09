5bb0d92839fc625e2ab448198265981b387992f4
Cristian Canales <55901424+Cristianc13@users.noreply.github.com>
2019-12-09 17:36:20 -0600

    Creacion de modelos(Vendedor-Cliente)

---
+++ HotelPrivado/HotelPrivado/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelPrivado.Models
+{
+    public class Cliente
+    {
+
+        [Key]
+        public int idCliente { get; set; }
+
+        public string nombre { get; set; }
+        public string documento { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime fechaNacimiento { get; set; }
+        public string lugarNacimiento { get; set; }
+        public string sexo { get; set; }
+        public string observacion { get; set; }
+
+
+    }
+}
---
+++ HotelPrivado/HotelPrivado/Models/Vendedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelPrivado.Models
+{
+    public class Vendedor
+    {
+        [Key]
+        public int idVendedor { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string observacion { get; set; }
+        public double sueldo { get; set; }
+
+
+
+
+    }
+}
