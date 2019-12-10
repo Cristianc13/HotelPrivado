7450308dad442d6d020cd8c49536e6adb76dafc6
AndruMeneses <56514362+AndruMeneses@users.noreply.github.com>
2019-12-09 18:02:37 -0600

    Creacion del modelo Alquiler

---
+++ HotelPrivado/HotelPrivado/Models/Alquiler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace HotelPrivado.Models
+{
+    public class Alquiler
+    {
+        [Key]
+        public int idAlquiler { get; set; }
+        public double Precio { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaEnt { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaSal { get; set; }
+        public string Observacion { get; set; }
+
+        //LLave foranea para Vendedor
+        [ForeignKey("Vendedor")]
+        public int idVendedor { get; set; }
+        [ForeignKey("idVendedor")]
+        public virtual Vendedor Vendedor { get; set; }
+
+        //LLave foranea para Cliente
+        [ForeignKey("Cliente")]
+        public int idCliente { get; set; }
+        [ForeignKey("idCliente")]
+        public virtual Cliente Cliente { get; set; }
+
+        //LLave foranea para Habitacion
+        [ForeignKey("Habitacion")]
+        public int idHabitacion { get; set; }
+        [ForeignKey("idHabitacion")]
+        public virtual Habitacion Habitacion { get; set; }
+    }
+}
