1fefd96457766bf07228a09e5ac8a2b031684818
Cristian Canales <55901424+Cristianc13@users.noreply.github.com>
2019-12-11 17:22:27 -0600

    Creacion del Panel

---
+++ HotelPrivado/HotelPrivado/Controllers/PanelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelPrivado.Controllers
+{
+    public class PanelController : Controller
+    {
+        public IActionResult Panel()
+        {
+            return View();
+        }
+
+        public IActionResult Panel2()
+        {
+            return View();
+        }
+    }
+}
