84fdc5c274d74de1786dc873f868391d290ded85
Cristian Canales <55901424+Cristianc13@users.noreply.github.com>
2019-12-11 16:03:43 -0600

    Creacion de scafolding de login

---
+++ HotelPrivado/HotelPrivado/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HotelPrivado.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HotelPrivado.Areas.Identity.IdentityHostingStartup))]
+namespace HotelPrivado.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
