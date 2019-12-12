1faa69201089de80637a0913ee92af375c64084c
Cristian Canales <55901424+Cristianc13@users.noreply.github.com>
2019-12-11 21:25:45 -0600

    Edicion del registrar

--- HotelPrivado/HotelPrivado/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HotelPrivado/HotelPrivado/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -67,7 +67,7 @@ namespace HotelPrivado.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Panel/Panel");
 
             if (ModelState.IsValid)
             {
